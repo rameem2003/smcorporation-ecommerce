2f4ade6177b944aadcf8b02fdaa27f7380176eac
rameem2003 <rameem2019@gmail.com>
2024-09-11 15:26:51 +0600

    banner update

--- src/components/screens/homepage/Banner.jsx
+++ src/components/screens/homepage/Banner.jsx
@@ -1,12 +1,14 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Slider from "react-slick";
 import banner from "../../../assets/banner.png";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Image from "../../common/Image";
+import axios from "axios";
 
 const Banner = () => {
   const [slide, setSlide] = useState(0);
+  const [bannerData, setBannerData] = useState({});
   const settings = {
     arrows: false,
     dots: true,
@@ -61,14 +63,23 @@ const Banner = () => {
       },
     ],
   };
+
+  const fetchData = async () => {
+    const res = await axios.get("https://smcorpapi.vercel.app/api/banner");
+    console.log(res.data[0]);
+    setBannerData(res.data[0]);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <section>
       <Slider {...settings}>
-        <Image src={banner} alt="banner" />
-        <Image src={banner} alt="banner" />
-        <Image src={banner} alt="banner" />
-        <Image src={banner} alt="banner" />
-        <Image src={banner} alt="banner" />
+        {bannerData?.banners?.map((data, i) => (
+          <Image className="w-full" src={data.url} alt="banner" />
+        ))}
       </Slider>
     </section>
   );
